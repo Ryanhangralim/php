b43604435bf7d3a239034e7ba8c897346eceff06
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-23 14:20:41 +0800

    Init index.php

---
+++ phpdasar/pertemuan3/index.php
@@ -0,0 +1,21 @@
+<?php 
+//Pengulangan : for, while, do..while, foreach: pengulahan khusus array
+
+// for($i = 0; $i < 5; $i++){
+//     echo "Hello World <br>";
+// }
+
+// $i = 0;
+// while($i < 5){
+//     echo "Hello World2 <br>";
+//     $i++;
+// }
+
+// $i = 0;
+
+// do {
+//     echo "Hello World3 <br>";
+//     $i++;
+// } while($i < 5);
+
+?>
