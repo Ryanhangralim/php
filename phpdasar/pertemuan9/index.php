7815563e33a582a96b03f1f27d1e6a9b1c1d1b85
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-24 14:03:16 +0800

    Update index.php

--- phpdasar/pertemuan9/index.php
+++ phpdasar/pertemuan9/index.php
@@ -1,3 +1,31 @@
+<?php 
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "phpdasar");
+
+// ambil data dari tabel / query data
+$result = mysqli_query($conn, "SELECT * FROM anime");
+// if( !$result ){
+//     echo mysqli_error($conn);
+// }
+
+// ambil data (fetch) dari object result
+// mysqli_fetch_row() : Mengembalikan array numerik
+// mysqli_fetch_assoc() : Mengembalikan array asosiatif
+// mysqli_fetch_array() :  Mengembalikan array numerik dan array asosiatif
+// mysqli_fetch_object() : Mengembalikan objek
+
+// $anime = mysqli_fetch_object($result);
+$anime_list = [];
+
+while ($anime = mysqli_fetch_assoc($result)){
+    $anime_list[] = $anime;
+}
+
+// var_dump($anime_list)
+
+$urut = 1;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,18 +49,22 @@
             <th>Tahun Rilis</th>
         </tr>
 
+        <?php foreach($anime_list as $anime_data):?>
         <tr>
-            <td>1</td>
+            <td><?= $urut?></td>
             <td>
                 <a href="">Ubah</a> |
                 <a href="">Hapus</a>
             </td>
-            <td><img src="img/frieren.jpg" alt=""  width="50"></td>
-            <td>Sousou no Frieren</td>
-            <td>series</td>
-            <td>28</td>
-            <td>2023</td>
+            <td><img src="img/<?= $anime_data["image"]; ?>" alt=""  width="50"></td>
+            <td><?= $anime_data["title"];?></td>
+            <td><?= $anime_data["type"];?></td>
+            <td><?= $anime_data["episode"];?></td>
+            <td><?= $anime_data["released_year"];?></td>
         </tr>
+        <?php
+        $urut++; 
+        endforeach;?>
 
     </table>
 </body>
