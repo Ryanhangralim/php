9eacd7de53e93075e9a16c44cc18b57dd7e296ea
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-24 13:24:19 +0800

    Init index.php

---
+++ phpdasar/pertemuan9/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+    <h1>Daftar Anime</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Judul</th>
+            <th>Tipe</th>
+            <th>Jumlah Episode</th>
+            <th>Tahun Rilis</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>
+                <a href="">Ubah</a> |
+                <a href="">Hapus</a>
+            </td>
+            <td><img src="img/frieren.jpg" alt=""  width="50"></td>
+            <td>Sousou no Frieren</td>
+            <td>series</td>
+            <td>28</td>
+            <td>2023</td>
+        </tr>
+
+    </table>
+</body>
+</html>
