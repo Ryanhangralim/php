8f0ffb62a33b86bbe2f755b3bd5661397c50fa15
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-25 20:59:56 +0800

    Add constructor

--- oophp/produk.php
+++ oophp/produk.php
@@ -7,6 +7,14 @@ class Produk{
     public $penerbit;
     public $harga;
 
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
     public function getLabel(){
         return "$this->judul, $this->penerbit";
     }
@@ -16,11 +24,11 @@ class Produk{
 // $produk1->judul = "Naruto";
 // var_dump($produk1)
 
-$produk3 = new Produk();
-$produk3->judul = "Naruto";
-$produk3->penulis = "Masashi Kisihimoto";
-$produk3->penerbit = "Shonen Jump";
-$produk3->harga = 30000;
+$produk3 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+// $produk3->judul = "Naruto";
+// $produk3->penulis = "Masashi Kisihimoto";
+// $produk3->penerbit = "Shonen Jump";
+// $produk3->harga = 30000;
 
 echo "Komik: $produk3->penulis, $produk3->penerbit";
 echo "<br>";
