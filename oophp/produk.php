32924d95a4674d2bd527750889c3cf45f1f7783f
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-25 20:50:53 +0800

    Add properties and methods

--- oophp/produk.php
+++ oophp/produk.php
@@ -2,10 +2,29 @@
 
 //jualan produk : komik & game
 class Produk{
-    
+    public $judul;
+    public $penulis;
+    public $penerbit;
+    public $harga;
+
+    public function getLabel(){
+        return "$this->judul, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1)
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kisihimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik: $produk3->penulis, $produk3->penerbit";
+echo "<br>";
+echo $produk3->getLabel();
+
 
 ?>
