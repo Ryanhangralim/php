b5d081742b0c96c90afd6e5ecb7b6c6558772af5
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-25 10:21:09 +0800

    init pertemuan14 files

---
+++ phpdasar/pertemuan15/index.php
@@ -0,0 +1,67 @@
+<?php 
+require 'functions.php';
+
+$anime_list = query("SELECT * FROM anime");
+
+//tombol cari ditekan
+if (isset($_POST["cari"])){
+    $anime_list = cari($_POST["keyword"]);
+}
+
+$urut = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+    <h1>Daftar Anime</h1>
+
+    <a href="tambah.php">Tambah data anime</a>
+    <br><br>
+
+    <form action="" method="post">
+
+        <input type="text" name="keyword", size="40" autofocus placeholder="masukkan keyword pencarian" autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+
+    </form>
+
+    <br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Judul</th>
+            <th>Tipe</th>
+            <th>Jumlah Episode</th>
+            <th>Tahun Rilis</th>
+        </tr>
+
+        <?php foreach($anime_list as $anime_data):?>
+        <tr>
+            <td><?= $urut?></td>
+            <td>
+                <a href="ubah.php?id=<?= $anime_data["id"]; ?>">Ubah</a> |
+                <a href="hapus.php?id=<?= $anime_data["id"]; ?>" onclick="return confirm('Apakah anda ingin menghapus data?')">Hapus</a>
+            </td>
+            <td><img src="img/<?= $anime_data["image"]; ?>" alt=""  width="50"></td>
+            <td><?= $anime_data["title"];?></td>
+            <td><?= $anime_data["type"];?></td>
+            <td><?= $anime_data["episode"];?></td>
+            <td><?= $anime_data["released_year"];?></td>
+        </tr>
+        <?php $urut++; ?>
+        <?php endforeach;?>
+
+    </table>
+</body>
+</html>
