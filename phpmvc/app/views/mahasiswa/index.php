716fdb19e2681bbecbba40487fd9ac06afc8927d
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-28 11:43:45 +0800

    Add search function

--- phpmvc/app/views/mahasiswa/index.php
+++ phpmvc/app/views/mahasiswa/index.php
@@ -7,12 +7,28 @@
     </div>
 
     <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6 mb-3">
             <!-- Button trigger modal -->
             <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
             Tambah Data Mahasiswa
             </button>
-            <br><br>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6 mb-3">
+            <form action="<?= BASEURL; ?>/mahasiswa/cari" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Cari Mahasiswa" name="keyword" id="keyword" autocomplete="off">
+                <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-6">
+
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
                 <?php foreach ($data["mhs"] as $mhs) : ?>
