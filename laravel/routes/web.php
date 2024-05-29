ca09d33ea377c13d92f86f24a5e952b948ef2623
Ryanhangralim <ryanhangralim@gmail.com>
2024-05-29 17:31:20 +0800

    Add categorycontroller

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,18 +36,7 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 //halaman categories
-Route::get('/categories', function(){
-    return view('categories', [
-        'title' => 'Post Categories',
-        'categories' => Category::all()
-    ]);
-});
+Route::get('/categories', [CategoryController::class, 'index']);
 
 //halaman category
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
-    ]);
-});
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
