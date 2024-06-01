d54af1212ffcb84265ba49fce6d2ac69f8593682
Ryanhangralim <ryanhangralim@gmail.com>
2024-06-01 21:28:17 +0800

    Add login and middleware

--- laravel/app/Http/Controllers/LoginController.php
+++ laravel/app/Http/Controllers/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -11,5 +13,34 @@ class LoginController extends Controller
         return view('login.index', [
             'title' => 'Login'
         ]);
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('/dashboard');
+        }
+
+        //balik ke halaman login
+        return back()->with('loginError', 'Login failed!');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -46,8 +47,14 @@ Route::get('/authors/{author:username}', [AuthorController::class, 'show']);
 Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
 
 //halaman login
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
 
 //halaman register
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+//halaman dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
